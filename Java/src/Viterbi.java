294228533ef45313794910c08b84a25723967ab7
DenizGuler <thetoasterofdoom1@gmail.com>
2017-09-20 23:00:01 -0400

    More of the same

--- Java/src/Viterbi.java
+++ Java/src/Viterbi.java
@@ -1,4 +1,14 @@
 
 
 public class Viterbi {
+
+
+    public double HMM_Viterbi(char state, int timeCourse){
+        if (state == 'B' && timeCourse == 0)
+            return 1;
+        if (timeCourse == 0)
+            return 0;
+
+        return 0.0;
+    }
 }
