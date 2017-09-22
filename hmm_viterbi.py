0808cc26afd5e8f9804a4d25222952bb2253b0bd
Valeriy Soltan <valeriy.soltan@gmail.com>
2017-09-21 23:44:09 -0400

    More of the same + 1 probability set for both emission states for B state at time course 0

--- hmm_viterbi.py
+++ hmm_viterbi.py
@@ -8,17 +8,25 @@ np = numpy
 
 states = {"B", "1", "2", "E"}
 
+start_state = "B"
+
 emissions = {"x", 'y'}
 
-# dictionary with all transitory probabilities
+# dictionaries with transitory probabilities ( the key is the destination and the title is the starting location)
 
-a = {"B1": 1, "B2": 0, "BE": 0, "11": 1 / 8, "12": 3 / 4, "1E": 1 / 8, "21": 1 / 2, "22": 1 / 4, "2E": 1 / 4}
+b = {"1" : 1, "2" : 0, "E" : 0}
+one = {"1" : 1/8, "2" : 3/4, "E": 1/8 }
+two = {"1" : 1/2, "2" : 1/4, "E" : 1/4}
+
+all = {"B" : b, "1" : one, "two" : two}
 
 # dictionary with the respective probabilities for emitted states x and y
 
 x = {"1": 7 / 8, "2": 1 / 16}
 
 y = {"1": 1 / 8, "2": 15 / 16}
+
+#####################################################
 
 # function that specifies all the base cases
 
@@ -28,7 +36,16 @@ def hmm_v(state, time):
 
     matrix = np.zeros((len(emissions), len(states), time + 1))
 
-    for i in range(time):
+    matrix[:, 0, 0 ] = 1
+
+    for i in range(0, time + 1):
+        for value in all.items():
+            print(value)
+
+
+
+
+
 
 
 
