103cc906c6d8684d7130ffa6ae76e1f537d5853e
Lulu Tian <lulutian@Lulus-Air.home>
2017-09-22 00:08:09 -0400

    Changed transition directories, wrote my thoughts on how to code the rest of it

--- hmm_viterbi.py
+++ hmm_viterbi.py
@@ -12,12 +12,13 @@ start_state = "B"
 
 emissions = {"x", 'y'}
 
-# dictionaries with transitory probabilities ( the key is the destination and the title is the starting location)
+# dictionaries with transitory probabilities ( the title is the destination and the key is the starting location)
+#   ex: dictionary named "one" holds keys to all the transition probabilities of going from other states TO one
 
-b = {"1" : 1, "2" : 0, "E" : 0}
-one = {"1" : 1/8, "2" : 3/4, "E": 1/8 }
-two = {"1" : 1/2, "2" : 1/4, "E" : 1/4}
-
+b = {"B":0, "1":0, "2":0, "E":0}
+one = {"B":1, "1":1/8, "2":1/2, "E":0}
+two = {"B":0, "1":3/4, "2":1/4,"E":0}
+e = {"B":0, "1":1/8, "2":1/4, "E":0}
 all = {"B" : b, "1" : one, "two" : two}
 
 # dictionary with the respective probabilities for emitted states x and y
@@ -38,7 +39,14 @@ def hmm_v(state, time):
 
     matrix[:, 0, 0 ] = 1
 
-    for i in range(0, time + 1):
+    for i in range(1, time + 1):
+        #for st in range(0, len(states)):
+         # matrix[1,st,i] =
+         # lulu in progress code:
+         # the idea is that for every current state in a time course:
+         # you go through all the previous states of the last time course
+         # and multiply by their transitions to the current state
+         # then take the max of these values for the one matrix[x,y,z] value
         for value in all.items():
             print(value)
 
