a2d6c040155ff9ecd877ebf7cd15de76a891edc6
DenizGuler <thetoasterofdoom1@gmail.com>
2017-09-21 21:40:34 -0400

    help

--- backward.py
+++ backward.py
@@ -29,9 +29,24 @@ def backward():
     dim = (dimx, dimy)
     matrix = np.zeros(dim)
     matrix[dimx - 1, dimy - 1] = 1
-    for i in reversed(range(1, dimy)):
+    for i in reversed(range(1, dimx - 1)):
+        if i in non_silent_states:
+            matrix[i, dimy - 1] = state_probs["3" + str(i)]  # * emission_probs[str(i) + emissions[0]]
+        else:
+            matrix[i, dimy - 1] = state_probs["3" + str(i)]
+    for r in range(0, dimy - 2):
+        for c in reversed(range(0, dimx-1)):
+            tot = 0
+            if r in non_silent_states:
+                for s in range(1, len(states) + 1):
+                    print(str(s) + str(r), str(c + 1))
+                    tot += matrix[s, c + 2] * state_probs[str(s) + str(r)] * emission_probs[str(s) + emissions[c + 1]]
 
-
+                    # matrix[s, r-1] * state_probs[str(s)+str(c)] * emission_probs[str(c)+emissions[r-1]]
+            else:
+                for s in reversed(range(1, len(states) + 1)):
+                    tot += matrix[s, c + 1] * state_probs[str(s) + str(c)]
+            matrix[r][c] = tot
     return matrix
 
 
@@ -45,10 +60,10 @@ st = [1, 2]
 nss = [1, 2]
 
 # dict of probabilities to switch between states (ex: 12 is the probability to move from state 1 to 2
-stp = {"01": 1, "02": 0, "11": 0.125, "12": 0.75, "13": 0.125, "22": 0.25, "21": 0.5, "23": 0.25}
+stp = {"31": 0.125, "32": 0.25, "11": 0.125, "12": 0.75, "22": 0.25, "21": 0.5, "10": 0, "20": 0}
 
 # dict of probabilities to emit from a non silent state (ex: 1X is the probability to emit X from state 1)
-ep = {"1X": 0.875, "1Y": 0.125, "2X": 0.0625, "2Y": 0.9375}
+ep = {"0X": 0, "1X": 0.875, "1Y": 0.125, "2X": 0.0625, "2Y": 0.9375}
 
 # list of emissions measured during the time frame
 e = ["X", "X", "X", "X"]
@@ -56,3 +71,4 @@ e = ["X", "X", "X", "X"]
 setup(sp, st, nss, e, stp, ep)
 
 print(backward())
+print(15/2**8)
