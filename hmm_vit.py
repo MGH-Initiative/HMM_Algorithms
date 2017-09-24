e933d79c20d712adac39200761b41d8520781d3e
Lulu Tian <lulutian@Lulus-Air.home>
2017-09-24 18:25:25 -0400

    Lulu new version of hmm viterbi. Mostly finished, but can’t run with numpy. It should return a matrix for the greatest probabilities for the states during the time course

---
+++ hmm_vit.py
@@ -0,0 +1,68 @@
+import numpy
+
+np = numpy
+
+###################################################
+
+# initialized and defined set of all potential states
+
+init_states = ["B", "1", "2","E"]
+states = ["1", "2","E"]
+num_states = 4
+current_state = "B"
+
+start_prob = 1
+
+# dictionaries with transitory probabilities ( the variable name is the starting location and the key is the end)
+#   ex: dictionary named "one" holds keys to all the transition probabilities of going from other states TO one
+
+b = {"B": 0, "1": 1, "2":0, "E":0}
+one = {"B": 0, "1": 1 / 8, "2": 3 / 4, "E": 1 / 8}
+two = {"B": 0, "1": 3/4, "2": 1/4,"E": 0}
+e = {"B": 0, "1": 1/8, "2": 1/4, "E": 0}
+
+allStates = [b, one, two, e]  # a vector of dictionaries
+
+# dictionary with the respective probabilities for emitted states x and y
+
+x = {"1": 7 / 8, "2": 1 / 16}
+
+y = {"1": 1 / 8, "2": 15 / 16}
+
+emissionCourse = [x,x,x]
+
+num_emissions = 2
+#####################################################
+
+# function that specifies all the base cases
+
+def hmm_vit(state, time):  # state is char and time is int
+
+    # three dimensional matrix that represents the table provided in the paper
+
+    matrix = np.zeros(len(init_states), time + 1)
+
+    # only possible state in time course 0 is B
+
+    matrix[0, 0] = start_prob
+
+    for i in range(1, time + 1):
+        for currst in states:
+            current_state = currst
+            temp = []
+            for prevst in range(num_states):
+                prevStateDictionary = allStates[prevst]
+                trans_prob = prevStateDictionary[current_state]
+                if currst!="E":
+                    currEmissions = emissionCourse[i-1]
+                    stateEmit = currEmissions[current_state]
+                    temp.append(trans_prob*stateEmit*matrix[prevst,i-1])
+                else:
+                    temp.append(trans_prob*matrix[prevst,i-1])
+            max_prob = max(temp)
+            matrix[init_states.index(current_state), i] = max_prob
+
+
+    return matrix
+
+print(hmm_vit("B", 4))
